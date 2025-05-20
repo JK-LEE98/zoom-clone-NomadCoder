cadb347073d48dac26f736fa7b1f0007a0f901c5
JK-LEE98 <abc9541@gmail.com>
2025-05-20 18:51:18 +0900

    rendering 셋업 완료

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(3000);
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log("Listning on http://localhost:3000");
+app.listen(3000, handleListen);
